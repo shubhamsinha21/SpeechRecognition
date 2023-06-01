53f2dbbeff671c9ad4982cf7f768e4cf77e8a03f
shubhamsinha21 <84564814+shubhamsinha21@users.noreply.github.com>
2023-06-01 16:49:23 +0530

    style update

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
 import React from 'react'
 import './App.css'
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition'
 const App = () => {
+
+  const startListening = () => {
+    return (
+      SpeechRecognition.startListening({ continuous: true, language: 'en-IN' })
+    )
+  }
+
+  const stopListening = () => {
+    return (
+      SpeechRecognition.stopListening()
+    )
+  }
+
+  const { transcript, browserSupportsSpeechRecognition } = useSpeechRecognition()
+  if (!browserSupportsSpeechRecognition) {
+    return null
+  }
+
   return (
     <>
       <div className="container">
         <h2>Speech to Text Converter</h2>
         <p>Hello world</p>
         <div className="main-content">
-
+          <p>{transcript}</p>
         </div>
 
         <div className="button-style">
           <button>Copy</button>
-          <button>Start Listening</button>
-          <button>Stop Listening</button>
+          <button onClick={startListening}>Start Listening</button>
+          <button onClick={stopListening}>Stop Listening</button>
         </div>
 
       </div>
